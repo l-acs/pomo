be95d3ea6134399674525afcca55862583c27f36
l-acs <lucas.sahar@mail.mcgill.ca>
2022-11-13 01:41:06 -0500

    Improve add-date.sh -- allow extending day past midnight

--- add-date.sh
+++ add-date.sh
@@ -1,1 +1,17 @@
-(d="$(date '+# %Y-%m-%d')" ; log="$HOME/.scripts/output/pomo.log" ; grep -x "$d" "$log" || echo "\n$d" >> "$log")
+#!/bin/sh
+
+if [ "$1" = "--extend" ] && [ -n "$2" ]; then
+   when="$2 hours ago"
+else
+   when=today
+fi
+
+# use ex. `--extend 4` to only have the date added if it's past 4am (otherwise, use the  yesterday)
+
+# if the `when` date is ill-formed, just use today's date
+d="$(date -d "$when" '+# %Y-%m-%d' 2>/dev/null)"
+log="$HOME/.scripts/output/pomo.log"
+
+if [ -n "$d" ] && [ -f "$log" ]; then
+   grep -xqE "^$d$" "$log" || echo "\n$d" >> "$log"
+fi
